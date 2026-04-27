21688a4a1b95a4d63292c16ff4bedcb221fa1c54
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-27 14:30:23 +0100

    added user alreay exception

---
+++ backend/src/main/java/com/budgetIt/budgetIt/exceptions/UserAlreadyExistExceptions.java
@@ -0,0 +1,7 @@
+package com.budgetIt.budgetIt.exceptions;
+
+public class UserAlreadyExistExceptions extends RuntimeException {
+    public UserAlreadyExistExceptions(String message) {
+        super(message);
+    }
+}
