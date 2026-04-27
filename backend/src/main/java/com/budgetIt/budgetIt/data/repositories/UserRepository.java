a338a2f70f69808c9bd383cbe8201bc698008072
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-27 14:19:43 +0100

    added user repository

---
+++ backend/src/main/java/com/budgetIt/budgetIt/data/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.budgetIt.budgetIt.data.repositories;
+
+
+import com.budgetIt.budgetIt.data.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    User findByEmail(String email);
+}
