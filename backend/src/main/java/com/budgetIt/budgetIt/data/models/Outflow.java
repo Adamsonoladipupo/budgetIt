f68f46228e2e3663c8ae46d333252ed96bff65d6
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-27 14:16:03 +0100

    added outflow class

---
+++ backend/src/main/java/com/budgetIt/budgetIt/data/models/Outflow.java
@@ -0,0 +1,41 @@
+package com.budgetIt.budgetIt.data.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+        import lombok.*;
+
+        import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Outflow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private OutflowType type;
+
+    private LocalDateTime date;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User user;
+
+    @PrePersist
+    public void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
