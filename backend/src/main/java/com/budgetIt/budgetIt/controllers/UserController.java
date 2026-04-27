b577a40a627f6e42e24d76e463be02a270a080a7
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-27 14:13:06 +0100

    added controller

---
+++ backend/src/main/java/com/budgetIt/budgetIt/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.budgetIt.budgetIt.controllers;
+
+import com.budgetIt.budgetIt.dtos.requests.LoginRequest;
+import com.budgetIt.budgetIt.dtos.requests.UserRequestRegistrationDTO;
+import com.budgetIt.budgetIt.dtos.responses.LoginResponse;
+import com.budgetIt.budgetIt.dtos.responses.UserResponseRegistrationDTO;
+import com.budgetIt.budgetIt.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseRegistrationDTO> register(@RequestBody UserRequestRegistrationDTO request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(userService.registerUser(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(userService.login(request));
+    }
+}
