2d4176c5589cef36516477b8a02fbcf3a992b632
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-27 14:34:35 +0100

    added the main application class

---
+++ backend/src/main/java/com/budgetIt/budgetIt/BudgetItApplication.java
@@ -0,0 +1,13 @@
+package com.budgetIt.budgetIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetItApplication.class, args);
+	}
+
+}
