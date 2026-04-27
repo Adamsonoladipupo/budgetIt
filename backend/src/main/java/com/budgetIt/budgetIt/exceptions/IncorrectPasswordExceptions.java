271a5d86c05b87c8f5ac57547545c86f62b96737
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-27 14:29:46 +0100

    added incorrect password exception

---
+++ backend/src/main/java/com/budgetIt/budgetIt/exceptions/IncorrectPasswordExceptions.java
@@ -0,0 +1,7 @@
+package com.budgetIt.budgetIt.exceptions;
+
+public class IncorrectPasswordExceptions extends RuntimeException {
+    public IncorrectPasswordExceptions(String message) {
+        super(message);
+    }
+}
